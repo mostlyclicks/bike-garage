b4d5e4890ed8415edad347ed7ca926b0a8ef028a
mostlyclicks <carlos@mostlyclicks.com>
2019-09-02 20:15:14 -0600

    fix config files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 // import { Link } from "gatsby"
 import { graphql } from "gatsby"
+// require('dotenv').config();
+// import { dotenv } from 'dotenv'
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
